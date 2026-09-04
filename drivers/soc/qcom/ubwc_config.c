bd7716ce095d8bdf842adfbef28ce8107660969d
sonic011gamer <efimgirjeu@gmail.com>
2026-09-04 20:53:16 +0300

    ubwc: Add 8992/8994 no_config.

--- drivers/soc/qcom/ubwc_config.c
+++ drivers/soc/qcom/ubwc_config.c
@@ -125,6 +125,8 @@ static const struct of_device_id qcom_ubwc_configs[] __maybe_unused = {
 	{ .compatible = "qcom,msm8956", .data = &no_ubwc_data },
 	{ .compatible = "qcom,msm8974", .data = &no_ubwc_data },
 	{ .compatible = "qcom,msm8976", .data = &no_ubwc_data },
+	{ .compatible = "qcom,msm8992", .data = &no_ubwc_data },
+	{ .compatible = "qcom,msm8994", .data = &no_ubwc_data },
 	{ .compatible = "qcom,msm8996", .data = &ubwc_1_0_hbb15 },
 	{ .compatible = "qcom,msm8998", .data = &ubwc_1_0_hbb15 },
 	{ .compatible = "qcom,qcm2290", .data = &ubwc_0_0_hbb15, },
